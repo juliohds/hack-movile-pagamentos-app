4f17cb31fdb8fab63f278cfc69eff686dad75356
juliohds <tecjuliohenrique@gmail.com>
2019-02-17 02:32:50 -0300

    ajustes layout

--- src/pages/property-detail/property-detail.ts
+++ src/pages/property-detail/property-detail.ts
@@ -1,64 +1,73 @@
-import {Component} from '@angular/core';
-import {ActionSheetController, ActionSheet, NavController, NavParams, ToastController} from 'ionic-angular';
-import {BrokerDetailPage} from '../broker-detail/broker-detail';
-import {PropertyService} from '../../providers/property-service-mock';
+import { Component } from "@angular/core";
+import {
+  ActionSheetController,
+  ActionSheet,
+  NavController,
+  NavParams,
+  ToastController
+} from "ionic-angular";
+import { BrokerDetailPage } from "../broker-detail/broker-detail";
+import { PropertyService } from "../../providers/property-service-mock";
 
 @Component({
-    selector: 'page-property-detail',
-    templateUrl: 'property-detail.html'
+  selector: "page-property-detail",
+  templateUrl: "property-detail.html"
 })
 export class PropertyDetailPage {
+  property: any;
 
-    property: any;
+  constructor(
+    public actionSheetCtrl: ActionSheetController,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public propertyService: PropertyService,
+    public toastCtrl: ToastController
+  ) {
+    this.property = this.navParams.data;
+    propertyService
+      .findById(this.property.id)
+      .then(property => (this.property = property));
+  }
 
-    constructor(public actionSheetCtrl: ActionSheetController, public navCtrl: NavController, public navParams: NavParams, public propertyService: PropertyService, public toastCtrl: ToastController) {
-        this.property = this.navParams.data;
-        propertyService.findById(this.property.id).then(
-            property => this.property = property
-        );
-    }
+  openBrokerDetail(broker) {
+    this.navCtrl.push(BrokerDetailPage, broker);
+  }
 
-    openBrokerDetail(broker) {
-        this.navCtrl.push(BrokerDetailPage, broker);
-    }
+  favorite(property) {
+    this.propertyService.favorite(property).then(property => {
+      let toast = this.toastCtrl.create({
+        message: "Property added to your favorites",
+        cssClass: "mytoast",
+        duration: 1000
+      });
+      toast.present(toast);
+    });
+  }
 
-    favorite(property) {
-        this.propertyService.favorite(property)
-            .then(property => {
-                let toast = this.toastCtrl.create({
-                    message: 'Property added to your favorites',
-                    cssClass: 'mytoast',
-                    duration: 1000
-                });
-                toast.present(toast);
-            });
-    }
+  share(property) {
+    let actionSheet: ActionSheet = this.actionSheetCtrl.create({
+      title: "Compartilhar via",
+      buttons: [
+        {
+          text: "Twitter",
+          handler: () => console.log("Compartilhar via twitter")
+        },
+        {
+          text: "Facebook",
+          handler: () => console.log("Compartilhar via facebook")
+        },
+        {
+          text: "Email",
+          handler: () => console.log("Compartilhar via email")
+        },
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => console.log("Cancelar")
+        }
+      ]
+    });
 
-    share(property) {
-        let actionSheet: ActionSheet = this.actionSheetCtrl.create({
-            title: 'Share via',
-            buttons: [
-                {
-                    text: 'Twitter',
-                    handler: () => console.log('share via twitter')
-                },
-                {
-                    text: 'Facebook',
-                    handler: () => console.log('share via facebook')
-                },
-                {
-                    text: 'Email',
-                    handler: () => console.log('share via email')
-                },
-                {
-                    text: 'Cancel',
-                    role: 'cancel',
-                    handler: () => console.log('cancel share')
-                }
-            ]
-        });
-
-        actionSheet.present();
-    }
-
+    actionSheet.present();
+  }
 }
