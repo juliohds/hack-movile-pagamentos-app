780693edda4f7e478a48237547a41637287d1c23
juliohds <tecjuliohenrique@gmail.com>
2019-02-17 08:50:02 -0300

    alerta

--- src/pages/broker-detail/broker-detail.ts
+++ src/pages/broker-detail/broker-detail.ts
@@ -1,20 +1,34 @@
-import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
-import {BrokerService} from '../../providers/broker-service-mock';
+import { Component } from "@angular/core";
+import { NavController, NavParams, AlertController } from "ionic-angular";
+import { BrokerService } from "../../providers/broker-service-mock";
+import { BrokerListPage } from "../broker-list/broker-list";
 
 @Component({
-    selector: 'page-broker-detail',
-    templateUrl: 'broker-detail.html'
+  selector: "page-broker-detail",
+  templateUrl: "broker-detail.html"
 })
 export class BrokerDetailPage {
+  broker: any;
 
-    broker: any;
+  constructor(
+    public alertController: AlertController,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public service: BrokerService
+  ) {
+    this.broker = this.navParams.data;
+    service.findById(this.broker.id).then(broker => (this.broker = broker));
+  }
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, public service: BrokerService) {
-        this.broker = this.navParams.data;
-        service.findById(this.broker.id).then(
-            broker => this.broker = broker
-        );
-    }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      title: "Crédito Liberado",
+      subTitle: "$100,00",
+      message: "adicionados a sua carteira.",
+      buttons: ["OK"]
+    });
 
+    await alert.present();
+    this.navCtrl.push(BrokerListPage);
+  }
 }
--- src/providers/mock-brokers.ts
+++ src/providers/mock-brokers.ts
@@ -18,66 +18,6 @@ let brokers: Array<any> = [
     email: "Valor a ser pago 103,00",
     picture:
       "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
-  },
-  {
-    id: 3,
-    name: "Aprovação de Crédito",
-    title: "R$ 100,00 - 14/12/2018",
-    phone: "Aceitar Oferta",
-    mobilePhone: "Recusar Empréstimo",
-    email: "Valor a ser pago 103,00",
-    picture:
-      "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
-  },
-  {
-    id: 4,
-    name: "Aprovação de Crédito",
-    title: "R$ 100,00 - 10/11/2018",
-    phone: "Aceitar Oferta",
-    mobilePhone: "Recusar Empréstimo",
-    email: "Valor a ser pago 103,00",
-    picture:
-      "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
-  },
-  {
-    id: 5,
-    name: "Aprovação de Crédito",
-    title: "R$ 100,00 - 19/10/2018",
-    phone: "Aceitar Oferta",
-    mobilePhone: "Recusar Empréstimo",
-    email: "Valor a ser pago 103,00",
-    picture:
-      "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
-  },
-  {
-    id: 6,
-    name: "Aprovação de Crédito",
-    title: "R$ 100,00 - 15/09/2018",
-    phone: "Aceitar Oferta",
-    mobilePhone: "Recusar Empréstimo",
-    email: "Valor a ser pago 103,00",
-    picture:
-      "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
-  },
-  {
-    id: 7,
-    name: "Aprovação de Crédito",
-    title: "R$ 100,00 - 18/08/2018",
-    phone: "Aceitar Oferta",
-    mobilePhone: "Recusar Empréstimo",
-    email: "Valor a ser pago 103,00",
-    picture:
-      "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
-  },
-  {
-    id: 8,
-    name: "Aprovação de Crédito",
-    title: "R$ 100,00 - 11/07/2018",
-    phone: "Aceitar Oferta",
-    mobilePhone: "Recusar Empréstimo",
-    email: "Valor a ser pago 103,00",
-    picture:
-      "https://banner2.kisspng.com/20180407/joe/kisspng-computer-icons-tax-payment-money-svg-5ac92b5bbe29f9.4347121415231332757789.jpg"
   }
 ];
 
